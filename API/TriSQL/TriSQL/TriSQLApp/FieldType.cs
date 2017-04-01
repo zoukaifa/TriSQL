cc2fd357d149c758a571299136109bec0ac9870d
邹开发 <1397645429@qq.com>
2017-04-02 00:46:36 +0800

    建立API的项目，以及API文档

---
+++ API/TriSQL/TriSQL/TriSQLApp/FieldType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriSQLApp {
+    /// <summary>
+    /// 定义了各个类型的宏
+    /// </summary>
+    class FieldType {
+        /// <summary>
+        /// 字符串
+        /// </summary>
+        public static int STRING = 1;
+
+        /// <summary>
+        /// 整数
+        /// </summary>
+        public static int INTEGER = 2;
+
+        /// <summary>
+        /// 浮点数
+        /// </summary>
+        public static int DOUBLE = 3;
+
+        /// <summary>
+        /// 日期类型 yyyy-MM-dd 格式
+        /// </summary>
+        public static int DATETIME = 4;
+
+        /// <summary>
+        /// 长整数
+        /// </summary>
+        public static int LONG = 5;
+
+        /// <summary>
+        /// CellId类型
+        /// </summary>
+        public static int CELLID = 6;
+    }
+}
---
+++ API/TriSQL/TriSQL/TriSQLApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trinity;
+using TriSQL;
+
+namespace TriSQLApp
+{
+    
+    internal class Program
+    {
+        
+        static void Tes(int a = 1, int b = 2, int c =3) { }
+        static void Main(string[] args)
+        {
+            bool a = true;
+            Tes(a: 1, c: 3);
+        }
+    }
+}
